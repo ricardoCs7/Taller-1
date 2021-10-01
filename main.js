13d531fcb11bfe6d724b89b8001245bff355d14c
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-10-01 16:46:22 -0300

    update

--- main.js
+++ main.js
@@ -17,13 +17,16 @@ function addComentario() {
         }
 
         if (commit) {
+
             const comments_array = JSON.parse(localStorage.getItem("comentarios"));
-            comments_array.push({ nombre, commit, fecha  });
+            comments_array.push({ nombre, commit, fecha });
             localStorage.setItem("comentarios", JSON.stringify(comments_array));
         }
         alert("comentario publicado")
     }
 }
+
+
 
 
 function getLocal() {
