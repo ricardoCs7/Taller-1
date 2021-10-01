e8bd1a4f61fdfd7bc0c4693a23673afffad8897d
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-10-01 01:33:25 -0300

    comentarios activos, falta agregar info de jugadores, validar la entrada de reviews

--- main.js
+++ main.js
@@ -2,33 +2,66 @@ document.addEventListener('DOMContentLoaded', getLocal, false);
 
 const comments_array = [];
 var box = document.getElementById("cajaComentarios");
-var commit = document.getElementById("commit");
+
 
 function addComentario() {
 
     var nombre = document.getElementById("nombre").value;
-    var comentario = document.getElementById("comentario").value;
+    var commit = document.getElementById("comentario").value;
 
 
-    if (!nombre == "" && !comentario == "") {
+    if (!nombre == "" && !commit == "") {
         if (localStorage.getItem("comentarios") == null) {
             localStorage.setItem("comentarios", "[]");
         }
 
-        if (comentario) {
+        if (commit) {
             const comments_array = JSON.parse(localStorage.getItem("comentarios"));
-            comments_array.push({ nombre, comentario });
+            comments_array.push({ nombre, commit });
             localStorage.setItem("comentarios", JSON.stringify(comments_array));
         }
         alert("comentario publicado")
     }
 }
 
-function getLocal() {
-    const comentario = JSON.parse(localStorage.getItem("comentarios"));
-    console.log(comentario);
 
-    box.innerHTML = JSON.parse(localStorage.getItem("comentarios"));
+function getLocal() {
+    let innerHTML = "";
+    JSON.parse(localStorage.getItem("comentarios")).forEach((index) => {
+ innerHTML += `
+ <div class="card text-left" id="com">
+ 
+   <div class="card-body">
+     <h4 class="card-title"> <img display: "inline-flex"; id="perfil" src="https://www.w3schools.com/howto/img_avatar.png" alt="Person" width="96" height="96">  ${index.nombre}</h4>
+     <p class="card-text">${index.commit}</p>
+   </div>
+ </div>
+ `;
+});
+box.innerHTML = innerHTML;
 }
+
+
+
+
+
+// function getLocal() {
+//     const comentario = localStorage.getItem("comentarios");
+
+//     for (let index = 0; index < JSON.parse(localStorage.comentarios).length; index++) {
+//         const element = JSON.parse(localStorage.getItem("comentarios"))[index];
+//         const container = document.createElement('div')
+//         const dvNombre = document.createElement('card-title');
+//         const dvMensaje = document.createElement('card-body');
+//         container.appendChild(dvNombre);
+//         container.appendChild(dvMensaje);
+//         box.appendChild(container);
+//         dvNombre.textContent = JSON.stringify(element.commit);
+//         dvMensaje.textContent = JSON.stringify(element.nombre);
+//     }
+
+
+
+// }
 
 
