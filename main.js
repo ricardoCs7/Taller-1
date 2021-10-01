a8249a754b106b9219e9323d5ebd68749f6b9ac2
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-10-01 16:24:54 -0300

    Comentarios se muestran del ultimo al primeo

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function addComentario() {
 
     var nombre = document.getElementById("nombre").value;
     var commit = document.getElementById("comentario").value;
+    var fecha = (new Date()).toLocaleString();
 
 
     if (!nombre == "" && !commit == "") {
@@ -17,7 +18,7 @@ function addComentario() {
 
         if (commit) {
             const comments_array = JSON.parse(localStorage.getItem("comentarios"));
-            comments_array.push({ nombre, commit });
+            comments_array.push({ nombre, commit, fecha  });
             localStorage.setItem("comentarios", JSON.stringify(comments_array));
         }
         alert("comentario publicado")
@@ -27,42 +28,21 @@ function addComentario() {
 
 function getLocal() {
     let innerHTML = "";
-    JSON.parse(localStorage.getItem("comentarios")).forEach((index) => {
- innerHTML += `
+    JSON.parse(localStorage.getItem("comentarios")).reverse().forEach((index) => {
+        innerHTML += `
  <div class="card text-left" id="com">
- 
    <div class="card-body">
      <h5 class="card-title"> <img display: "inline-flex"; id="perfil" src="https://www.w3schools.com/howto/img_avatar.png" alt="Person" width="96" height="96">  ${index.nombre}</h4>
      <p class="card-text">${index.commit}</p>
-     <p class="card-text">${Date().toLocaleString()}</p>
+     <p class="card-text">${index.fecha}</p>
    </div>
  </div>
  `;
-});
-box.innerHTML = innerHTML;
+    });
+    box.innerHTML = innerHTML;
 }
 
 
 
-
-
-// function getLocal() {
-//     const comentario = localStorage.getItem("comentarios");
-
-//     for (let index = 0; index < JSON.parse(localStorage.comentarios).length; index++) {
-//         const element = JSON.parse(localStorage.getItem("comentarios"))[index];
-//         const container = document.createElement('div')
-//         const dvNombre = document.createElement('card-title');
-//         const dvMensaje = document.createElement('card-body');
-//         container.appendChild(dvNombre);
-//         container.appendChild(dvMensaje);
-//         box.appendChild(container);
-//         dvNombre.textContent = JSON.stringify(element.commit);
-//         dvMensaje.textContent = JSON.stringify(element.nombre);
-//     }
-
-
-
-// }
 
 
